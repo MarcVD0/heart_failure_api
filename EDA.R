f4de833822a97a410f93a10444cf57db831f01cf
Maria Artigas <mariaartigas8@gmail.com>
2026-01-16 16:00:01 +0100

    Visualization variables

--- EDA.R
+++ EDA.R
@@ -5,37 +5,41 @@ library(ggcorrplot)
 
 df <- read.csv("heart_failure_clinical_records_dataset.csv")
 
-# variables binarias como factor
+# variables binarias como categoricas
 cat_vars <- c("anaemia","diabetes","high_blood_pressure","sex","smoking","DEATH_EVENT")
 df[cat_vars] <- lapply(df[cat_vars], factor)
 
 # Descripcion general del dataset
 summary(df)
-str(df)
+str(df) #que variables y como estan codificadas
 table(df$DEATH_EVENT)
 
-#Histogramas de variables continuas
+#variables continuas
 cont_vars <- c("age","creatinine_phosphokinase","ejection_fraction",
                "platelets","serum_creatinine","serum_sodium")
+# Histograms
+print(
+  df %>%
+    pivot_longer(all_of(cont_vars)) %>%
+    ggplot(aes(value)) +
+    geom_histogram(fill="steelblue", color="black") +
+    facet_wrap(~name, scales="free") +
+    theme_minimal()
+)
 
-df %>%
-  pivot_longer(all_of(cont_vars)) %>%
-  ggplot(aes(value)) +
-  geom_histogram(fill="steelblue", color="black") +
-  facet_wrap(~name, scales="free") +
-  theme_minimal()
+# Boxplots
+print(
+  df %>%
+    pivot_longer(all_of(cont_vars)) %>%
+    ggplot(aes(x = DEATH_EVENT, y = value, fill = DEATH_EVENT)) +
+    geom_boxplot() +
+    facet_wrap(~name, scales="free") +
+    theme_minimal()
+)
 
-#Boxplots por DEATH_EVENT
-df %>%
-  pivot_longer(all_of(cont_vars)) %>%
-  ggplot(aes(x = DEATH_EVENT, y = value, fill = DEATH_EVENT)) +
-  geom_boxplot() +
-  facet_wrap(~name, scales="free") +
-  theme_minimal()
-
-#Correlacion de variables numericas
+# Correlation plot
 corr <- cor(df[, sapply(df, is.numeric)])
-ggcorrplot(corr, lab = TRUE)
+print(ggcorrplot(corr, lab = TRUE))
 
 #Comparacion con tablas
 aggregate(. ~ DEATH_EVENT, data=df[, c("DEATH_EVENT","age","ejection_fraction","serum_creatinine")], mean)
